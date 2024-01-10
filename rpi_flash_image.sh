be637aefc0a1d647faa01a10ef3c6df8a1975625
Vlad <427departament@gmail.com>
2024-01-10 11:06:25 +0300

    Test write image with variables

--- rpi_flash_image.sh
+++ rpi_flash_image.sh
@@ -1,20 +1,20 @@
 #!/usr/bin/bash
 
 # Get image name
-image=`ls -la /home/rock/ | grep "img"`
+image=`ls /home/rock/ | grep "img"`
 
-# Message
+# Message log
+echo "Image name: $image"
 echo "Start write image" > /home/rock/rpi_flash_image/flash.log
 echo "Start write image"
 
 # Write image on rock pi
-echo $image
-#sudo dd if=/home/rock/image_S300_Tender_101023.img of=/dev/mmcblk1 bs=8M status=progress
-#sudo dd if=/home/rock/$image of=/dev/mmcblk1 bs=8M status=progress
+sudo dd if=/home/rock/$image of=/dev/mmcblk1 bs=8M status=progress
 
-# Message
+# Message log
 echo "End write image" > /home/rock/rpi_flash_image/flash.log
 echo "End write image"
 
 # Shutdown system
-#sudo shutdown -h now
+echo "Shutdown system"
+sudo shutdown -h now
