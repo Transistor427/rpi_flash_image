e8f3a0a374279e005fab4b1b57895ee58ce82bca
Vlad <427departament@gmail.com>
2024-01-10 10:55:27 +0300

    Modify install script

--- install.sh
+++ install.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/bash
+echo "[+] Copy script on init.d"
 sudo cp /home/rock/rpi_flash_image/rpi_flash_image.sh /etc/init.d/
+echo "[+] Changing access"
 sudo chmod +x /etc/inid.d/rpi_flash_image.sh
+echo "[+] Running the default script"
 sudo update-rc.d rpi_flash_image.sh defaults
+echo "[+] Backup rc.local"
 sudo mv /etc/rc.local /etc/rc.local.bak
+echo "[+] Copy modify rc.local"
 sudo cp /home/rock/rpi_flash_image/rc.local /etc/rc.local
+echo "[+] Installation completed successfully... Exit."
