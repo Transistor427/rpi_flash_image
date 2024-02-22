bbd943716337c1792735e3dbb7c5aacd05ad2b6f
Vlad <427departament@gmail.com>
2024-02-22 23:33:49 +0300

    Modify exec shell

--- rpi_flash_image.sh
+++ rpi_flash_image.sh
@@ -11,6 +11,7 @@ echo "Start write image"
 # Write image on rock pi
 sudo dd if=/home/rock/$image of=/dev/mmcblk1 bs=8M status=progress
 
+
 # Message log
 echo "End write image" > /home/rock/rpi_flash_image/flash.log
 echo "End write image"
